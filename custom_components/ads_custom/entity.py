1136d6c1828f32f78ed89814e8a46e6e76cceb18
Aaron Eisele <aaroneisele55@gmail.com>
2026-02-12 11:52:01 +0100

    add entity options

--- custom_components/ads_custom/entity.py
+++ custom_components/ads_custom/entity.py
@@ -21,6 +21,11 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class AdsEntity(Entity):
+        @staticmethod
+        def async_get_entity_options_flow(entity, platform):
+            """Return the entity options flow handler class for this entity."""
+            from .entity_options_flow import AdsEntityOptionsFlowHandler
+            return AdsEntityOptionsFlowHandler
     """Representation of ADS entity."""
 
     _attr_should_poll = False
