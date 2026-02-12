d5e47a30e426ac25ea97320b860bf09cf23228dd
Aaron Eisele <aaroneisele55@gmail.com>
2026-02-12 13:32:42 +0100

    fix syntax error

--- custom_components/ads_custom/entity.py
+++ custom_components/ads_custom/entity.py
@@ -17,14 +17,11 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class AdsEntity(Entity):
-<<<<<<< HEAD
     @staticmethod
     def async_get_entity_options_flow(entity, platform):
         """Return the entity options flow handler class for this entity."""
         from .entity_options_flow import AdsEntityOptionsFlowHandler
         return AdsEntityOptionsFlowHandler
-=======
->>>>>>> 4c527a75d1df252cba2fe256e4113e54410b50e3
     """Representation of ADS entity."""
 
     @staticmethod
